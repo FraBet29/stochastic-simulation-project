2cfdefa75d8fa6934187b097d5603c327a11a24c
unknown <sophie.lequeu@student.uclouvain.be>
2023-12-23 12:10:55 +0100

    Correcting interval for Legpolyns and adding plotting fct

--- question2.py
+++ question2.py
@@ -75,7 +75,8 @@ def MCLS (samples, f, n):
     """
     # compute the Vandermonde matrix
     x = np.random.uniform(0, 1, len(samples))
-    V = np.polynomial.legendre.legvander(x, n)
+    x2 = x*2 - 1
+    V = np.polynomial.legendre.legvander(x2, n)
     """
     # compute the Vandermonde matrix by hand
     coef = [1] * (n + 1)
@@ -111,20 +112,69 @@ def MCLS_prime (samples, f):
     return estim
 
 
-def least_squares(n, M): # n + 1 coefficients, M samples
+def least_squares(n, M): 
+    """
+    Compute n + 1 coefficients of least squares based on M samples
+    args : n, st n+1 is the number of coefficients
+           M, number of samples to use
+    return : c, vector containing the n+1 optimal coefficients
+    """
 	# generate M samples
-	x = np.random.uniform(0, 1, M)
-	y = f(x)
+    x = np.random.uniform(0, 1, M)
+    y = f(x)
 	# solve the least squares problem
-	c = np.polynomial.legendre.Legendre.fit(x, y, n, domain=[0, 1]).coef
-	return c
+    c = np.polynomial.legendre.Legendre.fit(x, y, n, domain=[0, 1]).coef
+    return c
 
 def MCLS_new(samples, f, n):
     c = least_squares(n, len(samples))
-    estim = np.sum(f(samples) - np.polynomial.legendre.legval(samples, c)) / len(samples) + c[0]
+    samples2 = samples*2 - 1
+    estim = np.sum(f(samples) - np.polynomial.legendre.legval(samples2, c)) / len(samples) + c[0]
     return estim
 
 def MCLS_prime_new(samples, f):
     c = least_squares(0, len(samples))
     estim = c
     return estim
+
+def multiple_loglog_graph(nb_samples, MC_estims_list, ref_value):
+    """
+    Plot the graph of the error in absolute value of the MC_estims for the 'true' value ref_value.
+    args : nb_samples, number of samples used to compute each estimator
+           MC_estims_list, list of the estimators
+           ref_value, the reference 'true' value
+    return : /
+    """
+    plt.figure(figsize=(8, 6))
+    colors = plt.cm.get_cmap('tab10', len(MC_estims_list))  # Get distinct colors
+
+    for i, MC_estims in enumerate(MC_estims_list):
+        # compute absolute values of the error between estimators and ref_value
+        absolute_errors = np.abs(MC_estims - ref_value)
+
+        # create log-log plot
+        log_nb_samples = np.log(nb_samples)
+        log_errors = np.log(absolute_errors)
+
+        plt.scatter(log_nb_samples, log_errors, label=f'Series {i+1}', color=colors(i))
+
+        # Fit a linear regression line
+        regression = LinearRegression()
+        regression.fit(log_nb_samples.reshape(-1, 1), log_errors)
+        pred = regression.predict(log_nb_samples.reshape(-1, 1))
+        plt.plot(log_nb_samples, pred, color=colors(i))
+
+        # Get the coefficients of the linear regression
+        slope = regression.coef_[0]
+        intercept = regression.intercept_
+        equation = f'y = {slope:.2f}x + {intercept:.2f}'
+        plt.text(0.5, -8 - i * 0.5, equation, fontsize=10, color=colors(i))
+
+    plt.xlabel('Log(Number of samples M)')
+    plt.ylabel('Log(Absolute error)')
+    plt.title('Log-log plot of absolute error \n as a function of the number of samples M')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    return
