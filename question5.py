fa8c971f0c5035f957921ecd01e7de3bf18f2294
Marianna <marianna.dellotto@mail.polimi.it>
2023-12-29 15:43:52 +0100

    draft 5.b

--- question5.py
+++ question5.py
@@ -62,3 +62,49 @@ def solve_FHN_scipy(epsilon, I, v0, w0, t0, T, Nt, a, b):
     # solve the system of ODEs
     sol = solve_ivp(fitzhugh_nagumo, [t0, T], y0, t_eval=t)
     return sol.y[0], sol.y[1], sol.t
+
+def calculate_Q(epsilon, I, v0, w0, t0, T, Nt, a, b):
+    """
+    Calculate the quantity of interest Q for the Fitzhugh-Nagumo model system of ODEs.
+    args: epsilon, parameter of the model
+            I, parameter of the model
+            v0, initial value of v
+            w0, initial value of w
+            t0, initial time
+            T, final time
+            Nt, number of time steps
+            a, parameter of the model
+            b, parameter of the model
+    """
+    v, w, t = solve_FHN(epsilon, I, v0, w0, t0, T, Nt, a, b)
+    dt = (T-t0)/Nt
+    return (np.sum(np.square(v))-v[0]**2-v[len(v)-1]**2)*dt
+
+
+def MCLS_multiple(a_samples, b_samples, n, epsilon, I, v0, w0, t0, T, Nt):
+    a = np.random.uniform(0.6, 0.8, len(a_samples))
+    x = a
+    y=np.zeros(len(a))
+    for i in range(len(a)):
+        y[i] = MCLS_interior(b_samples, n, epsilon, I, v0, w0, t0, T, Nt, a[i])
+    c = np.polynomial.legendre.Legendre.fit(x, y, n, domain=[0, 1]).coef
+    samples2 = a_samples*2 - 1
+    interior_integral= np.zeros(len(a_samples))
+    for i in range(len(a_samples)):
+        interior_integral[i] = MCLS_interior(b_samples, n, epsilon, I, v0, w0, t0, T, Nt, a_samples[i])
+    estim = np.sum(interior_integral - np.polynomial.legendre.legval(samples2, c)) / len(a_samples) + c[0]
+    return estim
+
+def MCLS_interior (b_samples, n, epsilon, I, v0, w0, t0, T, Nt, a):
+    b = np.random.uniform(0.7, 0.9, len(b_samples))
+    x = b
+    y=np.zeros(len(b))
+    for i in range(len(b)):
+        y[i] = calculate_Q(epsilon, I, v0, w0, t0, T, Nt, a, b[i])
+    c = np.polynomial.legendre.Legendre.fit(x, y, n, domain=[0, 1]).coef
+    samples2 = b_samples*2 - 1
+    Q = np.zeros(len(b_samples))
+    for i in range(len(b_samples)):
+        Q[i] = calculate_Q(epsilon, I, v0, w0, t0, T, Nt, a, b_samples[i])
+    estim = np.sum(Q - np.polynomial.legendre.legval(samples2, c)) / len(b_samples) + c[0]
+    return estim
